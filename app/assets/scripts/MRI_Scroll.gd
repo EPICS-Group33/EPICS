8f938fefef15a36585a2707e2947512b83a4c013
Rishikesh M <rishikesh.m2021@vitbhopal.ac.in>
2024-03-05 20:43:02 +0530

    Daily Commit (05/03/2024)
    Godot UI update

--- app/assets/scripts/MRI_Scroll.gd
+++ app/assets/scripts/MRI_Scroll.gd
@@ -1,7 +1,7 @@
 extends Sprite2D
 
 var frame_index = 0
-var frame_count = 36  
+var frame_count = 36
 
 func _input(event):
 	if event is InputEventMouseButton:
@@ -9,8 +9,8 @@ func _input(event):
 			frame_index += 1
 		elif event.button_index==MOUSE_BUTTON_WHEEL_UP:
 			frame_index -= 1
-		
-	   
+
+
 		frame_index = wrapf(frame_index, 0, frame_count)
-		
+
 		frame = frame_index
